a493df48b0cedd2ca3d09c6f4796c5fa762e54af
alivel1216 <akvelastegui1@espe.edu.ec>
2021-03-03 19:23:07 -0500

    Random correct and incorrect answers

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var respuestaIncorrecta;
 let preguntas = new Array(10);
 let resCorrect = new Array(10);
 let resIncorrect = new Array(10);
+let posRespuestas = new Array();
 var c = -1;
 var d = -2;
 var numPreg;
@@ -128,7 +129,7 @@ function jugando() {
             mostrar("ask");
             mostrar("opRes");
             ocultar("reiniciarJuego");
-            generarPregResp(numPreg);
+            numPos(numPreg);
             console.log("estado" + estado);
         }
         else if (juego2 === true) {
@@ -140,7 +141,7 @@ function jugando() {
             ocultarF();
             mostrar("ask");
             mostrar("opRes");
-            generarPregResp(numPreg);
+            numPos(numPreg);
             console.log("estado" + estado);
         }
         else if (juego3 === true) {
@@ -152,7 +153,7 @@ function jugando() {
             ocultarF();
             mostrar("ask");
             mostrar("opRes");
-            generarPregResp(numPreg);
+            numPos(numPreg);
             console.log("estado" + estado);
         } else if (juego4 === true) {
             estado = 0;
@@ -163,7 +164,7 @@ function jugando() {
             ocultarF();
             mostrar("ask");
             mostrar("opRes");
-            generarPregResp(numPreg);
+            numPos(numPreg);
             console.log("estado" + estado);
         }
     }    
@@ -179,7 +180,45 @@ function jugando() {
 
     
 }
-function generarPregResp(num) {
+
+function numPos(num){
+    for(var p=0;p<num;p++){
+        posRespuestas[p]=p;
+    }
+    var i,j,k;
+    for (i = posRespuestas.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        k = posRespuestas[i - 1];
+        posRespuestas[i - 1] = posRespuestas[j];
+        posRespuestas[j] = k;
+        
+    }
+
+    for(i=0;i<num;i++){
+        var x = 1 + Math.round(9 * Math.random());
+        var y = 1 + Math.round(9 * Math.random());
+        respuestaIncorrecta = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random()));
+        respuestaCorrecta = x * y;
+        if(posRespuestas[i]%2==0){            
+            resCorrect[i]=respuestaCorrecta;//
+        }else{           
+            resCorrect[i]=respuestaIncorrecta;
+        }
+
+        preguntas[i] = x + "*" + y + "=" + resCorrect[i];
+
+        //Mostrar en la tabla
+        var fila = document.createElement("tr");
+        var listPreguntas = document.createElement("th");
+        listPreguntas.textContent = preguntas[i];
+        fila.appendChild(listPreguntas);
+        list.appendChild(fila);
+    }
+    console.log(resCorrect);
+    console.log(posRespuestas);
+    console.log("Preguntas"); console.log(preguntas);
+}
+/*function generarPregResp(num) {
 
     for (let i = 0; i < num; i++) {
         var x = 1 + Math.round(9 * Math.random());
@@ -188,7 +227,11 @@ function generarPregResp(num) {
         respuestaCorrecta = x * y;
         resCorrect[i] = respuestaCorrecta;
         resIncorrect[i] = respuestaIncorrecta;
-        preguntas[i] = x + "*" + y + "=" + respuestaIncorrecta;
+        //Posiciones de respuestas correctas e Incorrectas en aleatorio
+        
+        preguntas[i] = x + "*" + y + "=" + resCorrect[i];
+        
+        //Fin
 
         var fila = document.createElement("tr");
         var listPreguntas = document.createElement("th");
@@ -196,10 +239,13 @@ function generarPregResp(num) {
         fila.appendChild(listPreguntas);
         list.appendChild(fila);
     }
-    console.log(resIncorrect);
-    console.log(resCorrect);
 
-}
+    console.log("respuestas:"+resCorrect);
+        console.log("posiciones"+posRespuestas);
+    /*console.log(resIncorrect);
+    console.log(resCorrect);*/
+
+//}
 function finalJuego(){
     mostrar("finJuego");
     mostrar("reiniciarJuego");
