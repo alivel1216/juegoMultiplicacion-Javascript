a0dd9c7082458c7c3688ef725488b567a0ab7296
alivel1216 <akvelastegui1@espe.edu.ec>
2021-03-04 06:19:40 -0500

    Final Version

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 window.onload = cantidadPreguntas;
-var estado = 1;
 var tiempoTotal;
 var tiempoJuego;
 var tiempoSancion;
 var list = document.querySelector(".showPre")
 var respuestaCorrecta;
 var respuestaIncorrecta;
-let preguntas = new Array(10);
-let resCorrect = new Array(10);
-let resIncorrect = new Array(10);
-let posRespuestas = new Array();
-var c = -1;
-var d = -2;
+let preguntas = [];
+let respuestas = [];
+let resCorrect = [];
+let posRespuestas = [];
+let allTiemposJuego = [];
+var c = 0;
+var d = -1;
 var numPreg;
+var inicio = 3;
 
 var juego1 = false;
 var juego2 = false;
@@ -20,7 +21,6 @@ var juego3 = false;
 var juego4 = false;
 
 function cantidadPreguntas() {
-    s=0;
     document.getElementById("opcion").addEventListener("click", j10P);
     document.getElementById("opcion2").addEventListener("click", j25P);
     document.getElementById("opcion3").addEventListener("click", j50P);
@@ -40,7 +40,8 @@ function cantidadPreguntas() {
 
     }
     function j25P() {
-        document.getElementById("opcion2").style.backgroundColor = "red";
+        document.getElementById("opcion2").style.backgroundColor = "rgb(108, 211, 177)";
+        document.getElementById("opcion2").style.color = "white";
         juego1 = false;
         juego2 = true;
         juego3 = false;
@@ -53,6 +54,7 @@ function cantidadPreguntas() {
     }
     function j50P() {
         document.getElementById("opcion3").style.backgroundColor = "rgb(108, 211, 177)";
+        document.getElementById("opcion3").style.color = "white";
         juego1 = false;
         juego2 = false;
         juego3 = true;
@@ -65,6 +67,7 @@ function cantidadPreguntas() {
     }
     function j99P() {
         document.getElementById("opcion4").style.backgroundColor = "rgb(108, 211, 177)";
+        document.getElementById("opcion4").style.color = "white";
         juego1 = false;
         juego2 = false;
         juego3 = false;
@@ -83,6 +86,11 @@ function mostrar(id) {
 function ocultar(id) {
     document.getElementById(id).style.display = "none";
 }
+function ocultarF() {
+    document.getElementById("opPre").style.display = "none";
+    document.getElementById("comenzarJuego").style.display = "none";
+}
+
 
 function empezar() {
     ocultar("opPre");
@@ -93,249 +101,222 @@ function empezar() {
     ocultar("comenzarJuego");
     mostrar("conteoJuego");
     conteo();
+    inicio = 3;
 }
+
 
 function conteo() {
-    jugando();
-    id = setInterval(jugando, 1000);
-}
-function jugando() {
-    var inicio = 3
-    inicio -= s;
-    s++;
-    document.getElementById("contador").innerHTML = inicio;
-    if (inicio === 0) {
-        inicio = 0;
-        ocultar("opPre");
-        ocultar("finJuego");
-        ocultar("comenzarJuego");
-        ocultar("conteoJuego");
-        ocultar("reiniciarJuego");
-        /*mostrar("ask");
-        mostrar("opRes");*/
 
-        /*location.reload();
-        ocultar("ask");
-        ocultar("opRes");
-        mostrar("comenzarJuego");
-        estado = 1;*/
-        if (juego1 === true) {
+    //jugando();
+    //id = setInterval(jugando, 1000);
 
-            estado = 0;
-            jugando = true;
-            tiempoTotal = 0;
-            tiempoJuego = 0;
-            tiempoSancion = 0;
-            mostrar("ask");
-            mostrar("opRes");
+    id = setInterval(function () {
+        //inicio = 3
+        inicio -= 1;
+        document.getElementById("contador").innerHTML = inicio;
+        if (inicio === 0) {
+            detenerConteo();
+            //inicio=0;
+            ocultar("opPre");
+            ocultar("finJuego");
+            ocultar("comenzarJuego");
+            ocultar("conteoJuego");
             ocultar("reiniciarJuego");
-            numPos(numPreg);
-            console.log("estado" + estado);
-        }
-        else if (juego2 === true) {
-            estado = 0;
-            jugando = true;
-            tiempoTotal = 0;
-            tiempoJuego = 0;
-            tiempoSancion = 0;
-            ocultarF();
-            mostrar("ask");
-            mostrar("opRes");
-            numPos(numPreg);
-            console.log("estado" + estado);
-        }
-        else if (juego3 === true) {
-            estado = 0;
-            jugando = true;
-            tiempoTotal = 0;
-            tiempoJuego = 0;
-            tiempoSancion = 0;
-            ocultarF();
-            mostrar("ask");
-            mostrar("opRes");
-            numPos(numPreg);
-            console.log("estado" + estado);
-        } else if (juego4 === true) {
-            estado = 0;
-            jugando = true;
-            tiempoTotal = 0;
-            tiempoJuego = 0;
-            tiempoSancion = 0;
-            ocultarF();
-            mostrar("ask");
-            mostrar("opRes");
-            numPos(numPreg);
-            console.log("estado" + estado);
-        }
-    }    
-    /*for (let i = 0; i < 10; i++) {
-           
-            if (resIncorrect[i] != resCorrect[i]) {
-                tiempoSancion += 0.5;
+            if (juego1 === true) {
+                jugando = true;
+                tiempoTotal = 0;
+                tiempoJuego = 0;
+                tiempoSancion = 0;
+                mostrar("ask");
+                mostrar("opRes");
+                ocultar("reiniciarJuego");
+                numPos(numPreg);
+                iniciarCuenta();
+                detenerConteo();
             }
-            //}
-            console.log(tiempoSancion)
-        }
-    }*/
+            else if (juego2 === true) {
+                jugando = true;
+                tiempoTotal = 0;
+                tiempoJuego = 0;
+                tiempoSancion = 0;
+                ocultarF();
+                mostrar("ask");
+                mostrar("opRes");
+                numPos(numPreg);
 
-    
+            }
+            else if (juego3 === true) {
+                jugando = true;
+                tiempoTotal = 0;
+                tiempoJuego = 0;
+                tiempoSancion = 0;
+                ocultarF();
+                mostrar("ask");
+                mostrar("opRes");
+                numPos(numPreg);
+
+            } else if (juego4 === true) {
+                jugando = true;
+                tiempoTotal = 0;
+                tiempoJuego = 0;
+                tiempoSancion = 0;
+                ocultarF();
+                mostrar("ask");
+                mostrar("opRes");
+                numPos(numPreg);
+
+            }
+        }
+
+    }, 1000)
 }
 
-function numPos(num){
-    for(var p=0;p<num;p++){
-        posRespuestas[p]=p;
+
+function detenerConteo() {
+    clearInterval(id);
+    //inicio=3;
+}
+function numPos(num) {
+    //Lleno un array con numeros de 0 a n
+    for (var p = 0; p < num; p++) {
+        posRespuestas[p] = p;
     }
-    var i,j,k;
+    var i, j, k;//variables auxiliares
+    //Bucle para desordenar los elementos del array
     for (i = posRespuestas.length; i; i--) {
         j = Math.floor(Math.random() * i);
         k = posRespuestas[i - 1];
         posRespuestas[i - 1] = posRespuestas[j];
         posRespuestas[j] = k;
-        
     }
-
-    for(i=0;i<num;i++){
+    //Bucle para crear las preguntas
+    for (i = 0; i < num; i++) {
         var x = 1 + Math.round(9 * Math.random());
         var y = 1 + Math.round(9 * Math.random());
         respuestaIncorrecta = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random()));
         respuestaCorrecta = x * y;
-        if(posRespuestas[i]%2==0){            
-            resCorrect[i]=respuestaCorrecta;//
-        }else{           
-            resCorrect[i]=respuestaIncorrecta;
+        //resCorrect[i]=respuestaCorrecta;
+        //Si la posicion del array desordenado es par almaceno las respuestas correctas y si es impar las incorrectas
+        if (posRespuestas[i] % 2 == 0) {
+            respuestas[i] = respuestaCorrecta;
+        } else {
+            respuestas[i] = respuestaIncorrecta;
         }
 
-        preguntas[i] = x + "*" + y + "=" + resCorrect[i];
+        preguntas[i] = x + "*" + y + "=" + respuestas[i];//En cada pregunta guarda la respuestavaria entre para e impar
 
-        //Mostrar en la tabla
+        //Mostrar las preguntas en una tabla
         var fila = document.createElement("tr");
         var listPreguntas = document.createElement("th");
         listPreguntas.textContent = preguntas[i];
         fila.appendChild(listPreguntas);
         list.appendChild(fila);
     }
-    console.log(resCorrect);
+    console.log("Respuestas Aleatorias");
+    console.log(respuestas);
+    console.log("Posiciones");
     console.log(posRespuestas);
-    console.log("Preguntas"); console.log(preguntas);
+    console.log("Preguntas");
+    console.log(preguntas);
+    console.log("Respuestas Correctas");
+    console.log(resCorrect);
 }
-/*function generarPregResp(num) {
 
-    for (let i = 0; i < num; i++) {
-        var x = 1 + Math.round(9 * Math.random());
-        var y = 1 + Math.round(9 * Math.random());
-        respuestaIncorrecta = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random()));
-        respuestaCorrecta = x * y;
-        resCorrect[i] = respuestaCorrecta;
-        resIncorrect[i] = respuestaIncorrecta;
-        //Posiciones de respuestas correctas e Incorrectas en aleatorio
-        
-        preguntas[i] = x + "*" + y + "=" + resCorrect[i];
-        
-        //Fin
+var blocks = document.getElementsByTagName('th');//Obtenemos las filas de la tabla
+//Funcion para pintar cada pregunta
+function pintarQuestion() {
+    c++;
+    if (c >= preguntas.length) {
+        finalJuego();
+        document.getElementById("reiniciarJuego").addEventListener("click", function () {
+            mostrar("opPre");
+            mostrar("comenzarJuego");
+            ocultar("finJuego");
+            ocultar("opRes");
+            ocultar("reiniciarJuego");
+            c = 0;
+            d = -1;
+            location.reload();
+            let nuevoTiempo = allTiemposJuego.push(tiempoTotal);
+            allTiemposJuego.sort((a, b) => a - b);
+            document.getElementById("10p").innerHTML = allTiemposJuego[0];
 
-        var fila = document.createElement("tr");
-        var listPreguntas = document.createElement("th");
-        listPreguntas.textContent = preguntas[i];
-        fila.appendChild(listPreguntas);
-        list.appendChild(fila);
+        });
+    } else {
+
+        blocks[c].style.backgroundColor = "rgb(151, 224, 200)";
     }
+}
 
-    console.log("respuestas:"+resCorrect);
-        console.log("posiciones"+posRespuestas);
-    /*console.log(resIncorrect);
-    console.log(resCorrect);*/
+function removerElements() {
 
-//}
-function finalJuego(){
+}
+//Funcion para despintar Pregunta 
+function despintarQuestion() {
+    d++;
+    if (d >= preguntas.length) {
+    } else {
+
+        blocks[d].style.backgroundColor = "";
+    }
+}
+var countRes = 0;//Contador que rrecorre cada pregunta
+var numError = 0;//contador que indica cuantos errores cometio
+//Evento al presionar boton Incorrecto
+document.getElementById("incorrecto").addEventListener("click", function (e) {
+
+    pintarQuestion();
+    despintarQuestion();
+    var recPos = posRespuestas[countRes];
+    if (recPos % 2 == 0) {
+        console.log("Incorrect-btnIn");
+        numError++;
+    } else {
+    }
+    countRes++;
+    console.log(numError);
+});
+document.getElementById("correcto").addEventListener("click", function (e) {
+
+    pintarQuestion();
+    despintarQuestion();
+    var recoPos = posRespuestas[countRes];
+    if (recoPos % 2 == 0) {
+        console.log("Correct-btnCor");
+
+    } else {
+        console.log("incorrect-btnCor");
+        numError++;
+    }
+    countRes++;
+    console.log(numError);
+});
+
+function finalJuego() {
     mostrar("finJuego");
     mostrar("reiniciarJuego");
     ocultar("ask");
     ocultar("opRes");
-    //location.reload();
+    tiempoSancion = 0.5 * numError;
+    console.log(numError);
+    tiempoTotal = tiempoJuego + tiempoSancion;
+    document.getElementById("tTotal").innerHTML = tiempoTotal + "s";
+    document.getElementById("tBase").innerHTML = tiempoJuego + "s";
+    document.getElementById("tPenalizacion").innerHTML = "+" + tiempoSancion + "s";
+
+    detenerCuenta();
+}
+
+function iniciarCuenta() {
+    accion = setInterval(function () {
+        tiempoJuego++;
+
+    }, 1000)
+
+}
+function detenerCuenta() {
+    clearInterval(accion);
 }
 
 
-var blocks = document.getElementsByTagName('th');
-
-    function pintarQuestion() {
-        c++;
-        if (c >= preguntas.length) {
-            finalJuego();
-            document.getElementById("reiniciarJuego").addEventListener("click", function (){
-                location.reload();
-            });
-        } else {
-
-            blocks[c].style.backgroundColor = "rgb(151, 224, 200)";
-        }
-    }
-
-    function despintarQuestion() {
-        d++;
-        if (d >= preguntas.length) {
-            estado = 3;
-        } else {
-
-            blocks[d].style.backgroundColor = "";
-        }
-
-    }
-
-    document.getElementById("incorrecto").addEventListener("click", function (e) {
-        //c++;
-        pintarQuestion();
-        despintarQuestion();
-        console.log("Estado: " + estado);
-
-    });
-    document.getElementById("correcto").addEventListener("click", function (e) {
-        //c++;
-        pintarQuestion();
-        despintarQuestion();
-    });
-
-    function ocultarF() {
-        document.getElementById("opPre").style.display = "none";
-        document.getElementById("comenzarJuego").style.display = "none";
-    }
-
-    function iniciarCuenta() {
-        accion = setInterval(function () {
-            tiempoJuego += 1;
-            if (tiemporestante == 0) {
-                detenerCuenta();
-                mostrar("gameover");
-                document.getElementById("gameover").innerHTML = "<p>Juego Finalizado!!</p> <p>Su puntaje es:" + puntaje + ".</p>";
-                ocultar("tiemporestante");
-                ocultar("correcto");
-                ocultar("error");
-                jugando = false;
-                document.getElementById("inicioreset").innerHTML = "Iniciar juego";
-
-
-            }
-
-        }, 1000)
-
-    }
-    function detenerCuenta() {
-        clearInterval(accion);
-    }
-/*document.getElementById("correcto").onclick = function () {
-    var blocks = document.getElementsByTagName('th');
-    blocks[c].style.backgroundColor = "rgb(151, 224, 200)";
-    console.log(preguntas[c]);
-    c++;
-    for (let i = 0; i < 10; i++) {
-
-        if (resIncorrect[i] != resCorrect[i]) {
-            tiempoSancion += 0.5;
-        }
-    }
-    if (blocks[blocks.length - 1]) {
-        estado = 2;
-        console.log("estado: " + estado)
-    }
-    console.log(tiempoSancion)
-}*/
 
